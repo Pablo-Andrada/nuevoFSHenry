818488f58eedcfdb7f8597f54c34476690cc83c7
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-11-08 04:37:14 -0300

    demo node

--- demo-node/index.js
+++ demo-node/index.js
@@ -5,6 +5,8 @@ const Repository = require("./models/repository");
 const newRepository = new Repository();
 
 newRepository.createActivity("Caminar", "hace bien a la salud");
+newRepository.createActivity("Correr", "hace bien a la salud");
+newRepository.createActivity("Saltar", "hace bien a la salud");
 
 console.log(newRepository.getAllActivities());
 
