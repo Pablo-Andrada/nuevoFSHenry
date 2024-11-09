1f9d5873e0a358fea0a62ceb39957f35dc818af9
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-11-09 05:58:16 -0300

    empezando con EXPRESS, scafolding

---
+++ demo-express/src/controllers/index.js
@@ -0,0 +1,7 @@
+//En este módulo van a estar definidas las funciones que establecen LA LÓGICA DE NEGOCIO de cada una de las rutas
+
+
+//Para esa ruta GET /users =>
+
+// vamos a ir a la base de datos a pedir informacion de los usuarios
+// y responder al cliente con la informacion obtenida
---
+++ demo-express/src/routes/index.js
@@ -0,0 +1,4 @@
+//aca van a estar definidas las rutas atraves de las cuales nos podemos comunicar
+
+
+//Tengo definida la solicitud a GET/users => controlador (se lo va a pedir al controlador)
