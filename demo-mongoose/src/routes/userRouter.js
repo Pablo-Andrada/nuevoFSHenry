25f09f6756d9417eaa22fbb5c307e5b66ba35412
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-11-13 08:57:40 -0300

    mas manejo de errores

--- demo-mongoose/src/routes/userRouter.js
+++ demo-mongoose/src/routes/userRouter.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const userControler = require("../controllers/userController");
 const validateUser = require("../middleware/validateUser");
+const validateId = require("../middleware/validateId");
 
 const userRouter = Router();
 
 userRouter.get("/", userControler.getAllUser);
 userRouter.get("/byName", userControler.getUserByName);
-userRouter.get("/:id", userControler.getUserById);
+userRouter.get("/:id",validateId, userControler.getUserById);
 userRouter.put("/addVehicle", userControler.addVehicle);
 userRouter.post("/", validateUser, userControler.createUser);
 
--- demo-mongoose/src/server.js
+++ demo-mongoose/src/server.js
@@ -13,4 +13,7 @@ app.use(miMiddleware);
 
 app.use(router);
 
+app.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).json({error: err.message})
+})
 module.exports = app;
