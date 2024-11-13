13bd2fd9cb306372f8e4025e6aef0379b6f6e6a4
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-11-13 08:08:51 -0300

    manejo de errores con catchAsync

--- demo-mongoose/src/controllers/userController.js
+++ demo-mongoose/src/controllers/userController.js
@@ -1,35 +1,35 @@
 const userService = require("../services/userService");
+const catchAsync = require("../utils/catchAsync");
 
+const getAllUser = async (req, res) => {
+    const users = await userService.getUsers();
+    res.status(200).json(users)
+};
+const getUserById = async (req, res) => {
+    const { id } = req.params;
+    const user = await userService.getUserById(id);
+    res.status(200).json(user);
+};
+const getUserByName = async (req, res) => {
+    const { name } = req.body;
+    const user = await userService.findUserByName(name);
+    res.status(200).json(user);
+};
+const addVehicle = async (req, res) => {
+    const { userId, vehicleId } = req.body;
+    await userService.addVehicle({ userId, vehicleId });
+    res.status(200).json({ message: "Todo correcto" })
+};
+const createUser = async (req, res) => {
+    const { name, email, age } = req.body;
+    const newUser = await userService.createUser({ name, email, age });
+    res.status(201).json(newUser)
+};
 
 module.exports = {
-    getAllUser: async (req, res) => {
-        try {
-            const users = await userService.getUsers();
-            res.status(200).json(users)
-        } catch (error) {
-            res.status(500).json({
-                error: "Error interno del servidor"
-            })
-        }
-    },
-    getUserById: async(req,res) => {
-        const { id } = req.params;
-        const user = await userService.getUserById(id);
-        res.status(200).json(user);
-    },
-    getUserByName: async (req,res) => {
-        const { name } = req.body;
-        const user = await userService.findUserByName(name);
-        res.status(200).json(user);
-    } ,
-    addVehicle: async(req,res) => {
-        const { userId,vehicleId } = req.body;
-        await userService.addVehicle({ userId, vehicleId });
-        res.status(200).json({message:"Todo correcto"})
-    },
-    createUser: async (req, res) => {
-        const { name, email, age } = req.body;
-        const newUser = await userService.createUser({ name, email, age });
-        res.status(201).json(newUser)
-    },
+    getAllUser: catchAsync(getAllUser),
+    getUserById: catchAsync(getUserById),
+    getUserByName: catchAsync(getUserByName),
+    addVehicle: catchAsync(addVehicle),
+    createUser: catchAsync(createUser),
 }
