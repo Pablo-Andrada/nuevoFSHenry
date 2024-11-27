62bb519fa0b6660189f44772f3e34e7d8d506fcf
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-11-27 05:33:47 -0300

    objetos con TypeScript

--- demo-typeScript/index.ts
+++ demo-typeScript/index.ts
@@ -1,16 +1,23 @@
-const nombre: string = "Jorge";
-
-const saludar = (name:string):string => {
-    return `Hola ${name}`
+interface IAddress{
+    street: string,
+    city: string,
 }
 
-console.log(saludar(nombre));
-
-saludar("Pedro");
-saludar("Maria");
-
-const calcularTotal = (quantity: number, price: number): number => {
-    return quantity * price;
+interface IUser {
+    name: string,
+    email: string,
+    age: number,
+    active: boolean,
+    address: IAddress,
 }
 
-console.log( calcularTotal(5, 10));
+const usuario1: IUser = {
+    name: "Pablo",
+    email: "pablo@gmail.com",
+    age: 35,
+    active: true,
+    address: {
+        street: "Calle falsa 123",
+        city: "Pilar",
+    }
+}
