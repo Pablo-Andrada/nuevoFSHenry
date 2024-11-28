3efbe193a3ea09ce39405dffe14f717c8e0a1487
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-11-28 10:39:43 -0300

    aprendiendo ENUM

--- demo-typeScript/dist/index.js
+++ demo-typeScript/dist/index.js
@@ -1,12 +1,36 @@
 "use strict";
-const nombre = "Jorge";
-const saludar = (name) => {
-    return `Hola ${name}`;
+// interface IUser {
+//     name: string,
+//     email: string,
+//     age: number,
+//     active: boolean,
+//     address: IAddress,
+// }
+// const usuario1: IUser = {
+//     name: "Pablo",
+//     email: "pablo@gmail.com",
+//     age: 35,
+//     active: true,
+//     address: {
+//         street: "Calle falsa 123",
+//         city: "Pilar",
+//     }
+// }
+var UserRole;
+(function (UserRole) {
+    UserRole["ADMIN"] = "admin";
+    UserRole["USER"] = "user";
+    UserRole["GUEST"] = "guest";
+})(UserRole || (UserRole = {}));
+const user1 = {
+    name: "Pablo",
+    email: "pablo@gmail.com",
+    age: 35,
+    active: true,
+    address: {
+        street: "Calle falsa 123",
+        city: "Pilar",
+    },
+    role: UserRole.USER
 };
-console.log(saludar(nombre));
-saludar("Pedro");
-saludar("Maria");
-const calcularTotal = (quantity, price) => {
-    return quantity * price;
-};
-console.log(calcularTotal(5, 10));
+console.log(user1);
--- demo-typeScript/index.ts
+++ demo-typeScript/index.ts
@@ -3,15 +3,41 @@ interface IAddress{
     city: string,
 }
 
-interface IUser {
+// interface IUser {
+//     name: string,
+//     email: string,
+//     age: number,
+//     active: boolean,
+//     address: IAddress,
+// }
+
+// const usuario1: IUser = {
+//     name: "Pablo",
+//     email: "pablo@gmail.com",
+//     age: 35,
+//     active: true,
+//     address: {
+//         street: "Calle falsa 123",
+//         city: "Pilar",
+//     }
+// }
+
+enum UserRole {
+    ADMIN = "admin",
+    USER = "user",
+    GUEST = "guest"
+}
+
+type TUser = {
     name: string,
     email: string,
     age: number,
     active: boolean,
     address: IAddress,
+    role: UserRole // "admin","user","guest"
 }
 
-const usuario1: IUser = {
+const user1: TUser = {
     name: "Pablo",
     email: "pablo@gmail.com",
     age: 35,
@@ -19,5 +45,8 @@ const usuario1: IUser = {
     address: {
         street: "Calle falsa 123",
         city: "Pilar",
-    }
+    },
+    role: UserRole.USER
 }
+
+console.log(user1);
