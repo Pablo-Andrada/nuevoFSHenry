77c287797f83f698b262c726d69cc3081a33b074
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-12-07 00:39:54 -0300

    css de App.jsx

--- demo-forms/vite-project/src/App.jsx
+++ demo-forms/vite-project/src/App.jsx
@@ -1,11 +1,33 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [userData, setUserData] = useState({
+    username: "",
+    password: "",
+  });
   
   return (
-    <>
-    </>
+    <form>
+      <h2>Login</h2>
+      <div>
+        <label>Username: </label>
+        <input
+          type="text"
+          value={userData.username}
+          name='username'
+          placeholder=''
+        />
+      </div>
+      <div>
+        <label>Password: </label>
+        <input type="text" />
+      </div>
+      <button>Submit</button>
+    </form>
   )
 }
 
-export default App
+export default App;
