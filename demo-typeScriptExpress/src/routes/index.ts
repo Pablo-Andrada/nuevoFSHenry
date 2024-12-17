864b409f22ae7678c705016e119f7b8a69b6275b
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-12-17 01:54:24 -0300

    auth middleware

--- demo-typeScriptExpress/src/routes/index.ts
+++ demo-typeScriptExpress/src/routes/index.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createUser,getUsers,deleteUser } from "../controllers/usersController";
+import auth from "../middlewares/auth";
 
 const router: Router = Router();
 
 router.post("/users", createUser);
 
-router.get("/users", getUsers);
+router.get("/users", auth, getUsers);
 
 router.delete("/users", deleteUser);
 
