6bcb25d8646c90099737403557d56635c3ce5a8d
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-01-25 05:32:32 -0300

    AJAX terminado

--- demo-ajax/index.js
+++ demo-ajax/index.js
@@ -1,14 +1,65 @@
 const addUserButton = document.getElementById("addUserButton");
 let i = 1;
 
+class User{
+    constructor(id, name,username, email) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+    }
+}
+
+
+class Repository {
+    constructor(){
+        this.users = []
+    }
+
+    createUser({id, name,username, email}) {
+        const newUser = new User(id, name, username, email);
+        this.users.push(newUser);
+    }
+}
+
+const newRepository = new Repository();
+
+const refresh = () => {
+    const usersContaniers = document.getElementById("usersContaniers");
+    usersContaniers.innerHTML = "";
+
+    const users = newRepository.users;
+
+    const htmlUsers = users.map((user) => {
+        const name = document.createElement("h3");
+        const email = document.createElement("p");
+
+        name.innerHTML = user.name;
+        email.innerHTML = user.email;
+
+        const card = document.createElement("div");
+        card.appendChild(name);
+        card.appendChild(email);
+
+        return card;
+    });
+
+    htmlUsers.forEach((card) => {
+        usersContaniers.appendChild(card);
+    })
+}
+
+
 const addUser = () => {
     if (i>10) {
         alert("No hay mas usuarios!")
     } else {
         $.get(`https://jsonplaceholder.typicode.com/users/${i}`, (data,status) => {
-            console.log(data);    
+            newRepository.createUser(data)               
+            i++;
+            refresh();
+            
         })
-        i++;
     }
 
 }
