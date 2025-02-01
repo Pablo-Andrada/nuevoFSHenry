74e01694cd537f54cb0aedc2beb30a791b6b88fd
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-02-01 19:13:30 -0300

    Empazando con boostrap

--- demo-typeScriptExpress/src/index.ts
+++ demo-typeScriptExpress/src/index.ts
@@ -2,6 +2,6 @@ import server from "./server";
 import { PORT } from "./config/envs";
 
 
-server.listen(3000, () => {
+server.listen(3001, () => {
     console.log(`Server listening on port ${PORT}`);    
 })
