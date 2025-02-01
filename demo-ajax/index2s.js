8c03d9729f5efa5b7392f98039716810d617cf93
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-02-01 18:18:38 -0300

    practicando2

--- demo-ajax/index2s.js
+++ demo-ajax/index2s.js
@@ -2,7 +2,39 @@ const addUserButton = document.getElementById('addUserButton');
 
 let i = 1;
 
-$.get(`https://jsonplaceholder.typicode.com/users/${i}`, (data,status) => {
-    console.log(data);
-    i++;
-})
+class User{
+    constructor(id, name,username, email) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+    }
+}
+
+class Repository{
+    constructor() {
+        this.users = [];    
+    }
+    createUser({id, name,username, email}) {
+        const newUser = new User(id, name, username, email);
+        this.users.push(newUser);
+    }
+}
+const newRepository = new Repository();
+
+
+
+
+const addUser = () => {
+    if (i > 10) { return alert('No hay mas usuarios') }
+    else {
+        $.get(`https://jsonplaceholder.typicode.com/users/${i}`, (data,status) => {
+            newRepository.createUser(data)
+            i++;
+            refresh();
+        })        
+    }
+}
+
+
+addUserButton.addEventListener('click',addUser)
