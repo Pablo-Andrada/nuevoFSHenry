a45fb9a66af7e8073dbbb8b56b9dba545b946d8e
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-02-03 04:25:14 -0300

    Practice

---
+++ demo-node/models/repositoryy.js
@@ -0,0 +1,19 @@
+const Activity = require("./activity");
+
+class Repository{
+    constructor() {
+        this.activities = [];
+        this.id = 0;
+    }
+
+    createActivity (title, description, imgUrl) {
+        const newActivity = new Activity(this.id, title, description, imgUrl);
+        this.id++;
+        this.activities.push(newActivity);
+    }
+    getAllActivities() {
+        return this.activities;
+    }
+}
+
+module.exports = Activity;
