b7376665f2bdf6c61326977ce5df90b5053ad787
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-02-06 14:12:38 -0300

    Creando index principal, routes y controllerss

--- demo-express2/src/controllers/index.js
+++ demo-express2/src/controllers/index.js
@@ -1,1 +1,5 @@
 //En este módulo estan las funciones que definen la LÓGICA DE NEGOCIO de cada una de las rutas
+
+
+//Para esa ruta GET/ users => vamos a ir a la base de datos a pedir la información de los usuarios y responder al cliente con la informacion obtenida.
+
