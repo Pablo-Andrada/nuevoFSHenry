08867181d7ff339706018f8ef2a074cb7b48d22e
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-02-07 00:32:23 -0300

    Repasando

--- demo-express2/src/controllers/index.js
+++ demo-express2/src/controllers/index.js
@@ -1,5 +1,9 @@
 //En este módulo estan las funciones que definen la LÓGICA DE NEGOCIO de cada una de las rutas
+//                                                            ||
+//                                      Me va a decir QUE tengo que hacer pero NO lo va hacer
 
 
-//Para esa ruta GET/ users => vamos a ir a la base de datos a pedir la información de los usuarios y responder al cliente con la informacion obtenida.
+//Para esa ruta GET/ users => 
+// Vamos a ir a la base de datos a pedir la información de los usuarios Y
+// Y responder al cliente con la informacion obtenida.
 
