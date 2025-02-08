cbe58693305da407ffdd41154be5e657e4c7c281
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-02-08 15:50:42 -0300

    Instalando middlewares

--- demo-express2/src/server.js
+++ demo-express2/src/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = require('./routes/index');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const server = express();
+
+// Acá quiero utilizar el middleware de morgan
+server.use(morgan('dev'));
+server.use(cors());
+server.use(express.json());
 
 server.use(router);
 
