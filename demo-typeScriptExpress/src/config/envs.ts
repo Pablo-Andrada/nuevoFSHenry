99c102cfdcea1a8a58e907a98c68720089c4d953
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-03-17 00:21:53 -0300

    Working

--- demo-typeScriptExpress/src/config/envs.ts
+++ demo-typeScriptExpress/src/config/envs.ts
@@ -1,7 +1,7 @@
 // import "dotenv/config";
 
-// export const PORT = process.env.PORT;
+// export const PORT = process.env.PORT
 
 import "dotenv/config";
 
-export const PORT = process.env.PORT;
+export const PORT = parseInt(process.env.PORT?.trim() || "3000", 10);
