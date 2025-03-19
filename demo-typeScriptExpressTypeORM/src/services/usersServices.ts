d4e70a5d7e1016ff6cb822785dc287a2ee970421
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-03-19 13:48:46 -0300

    Working

--- demo-typeScriptExpressTypeORM/src/services/usersServices.ts
+++ demo-typeScriptExpressTypeORM/src/services/usersServices.ts
@@ -6,21 +6,26 @@ import IUser from "../interfaces/IUser";
 let users: IUser[] = [];
 let id: number = 1;
 
-export const createUserService = async (userData: UserDto): Promise<IUser> => { 
-    // recibir los datos del usuario
-    // crear un nuevo usuario
-    const newUser: IUser = {
-        id,
-        name: userData.name,
-        email: userData.email,
-        age:userData.age,
-        active: userData.active
-    }
-    // incluir al nuevo usuario dentro dle arreglo
-    users.push(newUser);
-    id++;
-    // retornar el objeto creado
-    return newUser;
+export const createUserService = async (userData: UserDto)  => { 
+    const user = await UserModel.create(userData);
+    const result = await UserModel.save(user)
+    return user;
+
+
+    // // recibir los datos del usuario
+    // // crear un nuevo usuario
+    // const newUser: IUser = {
+    //     id,
+    //     name: userData.name,
+    //     email: userData.email,
+    //     age:userData.age,
+    //     active: userData.active
+    // }
+    // // incluir al nuevo usuario dentro dle arreglo
+    // users.push(newUser);
+    // id++;
+    // // retornar el objeto creado
+    // return newUser;
 };
 
 export const getUsersService = async ()  => {
