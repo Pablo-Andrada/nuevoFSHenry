92b5e545e4d770ae18dc8647b58cca4e4b0f08b1
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-04-01 02:40:31 -0300

    Working

--- demo-reactIntro/vite-project/src/App.jsx
+++ demo-reactIntro/vite-project/src/App.jsx
@@ -1,25 +1,13 @@
 import './App.css'
-import CardList from './CardList'
 import React, { useState, useEffect } from 'react';
 
 
 function App() {
-  
-  const [contador, setContador] = useState(0);
-
-
-  useEffect(() => {
-    console.log("El contador cambió:", contador);    
-  },[contador]);
-
   return (
-  <div>
-      <button onClick={()=>{setContador(contador + 1)}}> + </button>
-      <p>Contador: {contador} </p>
-      <button onClick={()=>{setContador(contador - 1)}}> - </button>
-
-  </div>
-  )
+    <div>
+      
+    </div>
+  ); 
 }
 
 export default App
